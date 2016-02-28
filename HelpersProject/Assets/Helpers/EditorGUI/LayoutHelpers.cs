aacf8f56f52b6750375398d66965aad2bc1a8f33
Denis Sylkin <densylkin@gmail.com>
2016-02-28 11:16:57 +0400

    First commit

---
+++ HelpersProject/Assets/Helpers/EditorGUI/LayoutHelpers.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+
+namespace UnityHelpers.GUI
+{
+    using UnityEngine;
+
+    public class VerticalBlock : IDisposable
+    {
+        public VerticalBlock(params GUILayoutOption[] options)
+        {
+            GUILayout.BeginVertical(options);
+        }
+
+        public VerticalBlock(GUIStyle style, params GUILayoutOption[] options)
+        {
+            GUILayout.BeginVertical(style, options);
+        }
+
+        public void Dispose()
+        {
+            GUILayout.EndVertical();
+        }
+    }
+
+    public class ScrollviewBlock : IDisposable
+    {
+        public ScrollviewBlock(ref Vector2 scrollPos, params GUILayoutOption[] options)
+        {
+            scrollPos = GUILayout.BeginScrollView(scrollPos, options);
+        }
+
+        public void Dispose()
+        {
+            GUILayout.EndScrollView();
+        }
+    }
+
+    public class HorizontalBlock : IDisposable
+    {
+        public HorizontalBlock(params GUILayoutOption[] options)
+        {
+            GUILayout.BeginHorizontal(options);
+        }
+
+        public HorizontalBlock(GUIStyle style, params GUILayoutOption[] options)
+        {
+            GUILayout.BeginHorizontal(style, options);
+        }
+
+        public void Dispose()
+        {
+            GUILayout.EndHorizontal();
+        }
+    }
+
+    public class ColoredBlock : IDisposable
+    {
+        public ColoredBlock(Color color)
+        {
+            GUI.color = color;
+        }
+
+        public void Dispose()
+        {
+            GUI.color = Color.white;
+        }
+    } 
+}
